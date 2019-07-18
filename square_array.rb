23926417d6b8ed2f69fce48d96f7819ab82a95f5
decent-token-6050 <garrettburns@mac.com>
2019-07-18 23:22:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,15 @@
-#def square_array(array)
- # array.each do |numbers|
-  #  numbers << numbers ** 2
-  #end
-  #return numbers
-#end
-
 def square_array(array)
-  squarednumber_array = []
-  array.each do |squarenumber|
-    squarednumber_array << squarenumber ** 2  
+  a = []
+  array.each do |numbers|
+    numbers << numbers ** 2
   end
-   return squarednumber_array
+  return numbers
 end
+
+#def square_array(array)
+ # squarednumber_array = []
+  #array.each do |squarenumber|
+   # squarednumber_array << squarenumber ** 2  
+  #end
+   #return squarednumber_array
+#end
